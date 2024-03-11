6ef060198e00ec404cf0554c07ee08a593f63b26
Brett Logan <lindluni@github.com>
2024-03-11 14:31:13 -0400

    Add ratelimiter comment
    Signed-off-by: Brett Logan <lindluni@github.com>

--- index.js
+++ index.js
@@ -171,8 +171,8 @@ const verifyMembership = async (req, res, next) => {
 
 const limiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
-    limit: 10, // limit each IP to 1 requests per windowMs
-    keyGenerator: (req) => req.body.issue.node_id,
+    limit: 10, // limit each IP to 10 requests per windowMs
+    keyGenerator: (req) => req.body.issue.node_id, // Rate limit based on the issue, allowing 10 requests per minute per issue (this is a failsafe to prevent abuse and should never be reached in normal operation)
     handler: (req, res, next, options) => {
         console.log(`[${req.key}] Rate limit exceeded`)
         return res.status(options.statusCode).send(options.message)
