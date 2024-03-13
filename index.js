f96640d37049bd25e04881625d4be3a25656893d
Brett Logan <lindluni@github.com>
2024-03-12 22:28:27 -0400

    Add startup debugging
    Signed-off-by: Brett Logan <lindluni@github.com>

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ import {
 const app = express()
 app.use(express.json())
 app.use(debugRequest)
-app.use(verifyIsPR)
-app.use(verifyIssueCommentCreatedEvent)
 app.use(hydrateKey)
 app.use(limiter)
+app.use(verifyIsPR)
+app.use(verifyIssueCommentCreatedEvent)
 app.use(verifyGitHubWebhook)
 app.use(verifyMembership)
 app.use(verifyCommand)
@@ -30,6 +30,7 @@ app.post('/api/github/webhooks', processWebhook)
 
 const main = async () => {
     const port = process.env.OSST_ACTIONS_BOT_PORT || process.env.PORT || 8080
+    console.log(`Starting server on port ${port}`)
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`)
     })
